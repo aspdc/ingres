2accaef377950f0e175bb325d9fe46d2f1a304d7
ni3rav <mahetan35@gmail.com>
2026-03-11 02:17:36 +0530

    add skills

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -16,6 +16,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Ingres | ASPDC</title>
+     <meta name="description" content="Smoother attendance for participants and Fewer spreadsheets for ASPDC volunteers" />
         <Meta />
         <Links />
       </head>
