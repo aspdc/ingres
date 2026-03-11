7c394291df8b3240247cfe868757da84f57ad0d7
ni3rav <mahetan35@gmail.com>
2026-03-11 13:12:17 +0530

    hello nextjs

---
+++ README.md
@@ -0,0 +1,23 @@
+# Ingress
+
+> Smoother attendance for participants and fewer spreadhsheets for 
+
+
+
+## Adding components
+
+To add components to your app, run the following command:
+
+```bash
+npx shadcn@latest add button
+```
+
+This will place the ui components in the `components` directory.
+
+## Using components
+
+To use the components in your app, import them as follows:
+
+```tsx
+import { Button } from "@/components/ui/button";
+```
---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import { Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const spaceGrotesk = Space_Grotesk({
+  variable: '--font-space-grotesk',
+  subsets: ['latin'],
+})
+
+
+export const metadata: Metadata = {
+  title: 'Ingress',
+  description: 'Smoother attendance for participants and fewer spreadhsheets for ASPDC volunteers',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${spaceGrotesk.className} overflow-x-hidden antialiased select-none`}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
